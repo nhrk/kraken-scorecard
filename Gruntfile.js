03cedc490afb6036137386bc022d2be9edfab2ba
nihark <nihar@Nihars-MacBook-Air.local>
2014-06-20 17:02:03 +0530

    Added Grunt tasks for easier deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,18 @@
 'use strict';
 
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+	console.log(require('path').resolve('tasks'));
+	// Load the project's grunt tasks from a directory
+	require('grunt-config-dir')(grunt, {
+		configDir: require('path').resolve('tasks')
+	});
 
-    // Load the project's grunt tasks from a directory
-    require('grunt-config-dir')(grunt, {
-        configDir: require('path').resolve('tasks')
-    });
-
-    // Register group tasks
-    grunt.registerTask('build', [ 'jshint', 'sass', 'requirejs', 'i18n', 'copyto' ]);
-    grunt.registerTask('test', [ 'jshint', 'mochacli' ]);
+	// Register group tasks
+	grunt.registerTask('build', ['jshint', 'sass', 'requirejs', 'i18n', 'copyto']);
+	grunt.registerTask('test', ['jshint', 'mochacli']);
+	grunt.registerTask('deploy', ['shell:git_pull', 'shell:npm_install', 'build']);
+	grunt.registerTask('reload', ['shell:pm2_reload']);
+	grunt.registerTask('bump', ['deploy', 'shell:pm2_reload']);
 
 };
